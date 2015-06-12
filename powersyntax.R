e9c14dcdbd920b6245ea8f5f09acf7ab621162ea
Josh Wondra <jdwondra@umich.edu>
2015-06-12 12:32:40 -0500

    - created wide form of data - added variable list to syntax

---
+++ powersyntax.R
@@ -0,0 +1,105 @@
+##### Power Thesis Syntax #####
+
+
+##### 1. Variable List #####
+
+## this variable list is based on the wide form of the data, but it can be used to understand the long form of the data as well
+## only the variables for the high power subject are listed, but the low power subject's variables can be accessed by changing the .hp to .lp
+
+## Session information and condition
+# dyadID: Dyad identification number
+# emot_cond: 1 = target is sad, 2 = target is angry
+# exper: experimenter's first name
+# subjectID.hp: Subject identification number
+
+## Now that you've finished reading the e-mail, what are your main feelings right now?
+# mainfeel1.hp, mainfeel2.hp, mainfeel3.hp, mainfeel_exp.hp: open-ended own feelings and explanation
+
+## After reading the e-mail, how much do you feel each of the following right now? 0 = not at all, 4 = extremely
+# happy.hp
+# symp.hp: sympathetic
+# angry.hp
+# compass.hp: compassionate
+# interest.hp: interested
+# disgust.hp: disgusted
+# mad.hp
+# anxious.hp
+# proud.hp
+# down.hp
+# worried.hp
+# frust.hp: frustrated
+# sad.hp
+# powerful.hp
+
+## What were the main feelings of the person who wrote the e-mail?
+# ofeel1.hp, ofeel2.hp, ofeel3.hp, ofeel_exp.hp: open-ended perceptions of the target's feelings
+
+## How much did the person who wrote the e-mail feel each of the following? 0 = not at all, 4 = extremely
+# thappy.hp
+# tshock.hp: shocked
+# thope.hp: hopeful
+# tangry.hp
+# tselfanger.hp: angry at self
+# tinterest.hp: interested
+# tdisgust.hp: disgusted
+# tmad.hp
+# tcurious.hp
+# tafraid.hp
+# tgrateful.hp
+# tproud.hp
+# tsurpr.hp: surprised
+# tamused.hp
+# tdown.hp
+# tworried.hp
+# tfrust.hp: frustrated
+# tsad.hp
+# tembarrass.hp: embarrassed
+# tguilty.hp
+# tconfused.hp
+# tpowerful.hp
+
+## Appraisals
+## Answer the following questions about the situation that was described in the e-mail (0 = not at all, 4 = extremely)
+# pleas.hp: How pleasant was it?
+# targag.hp: How much do you feel that the person who wrote the e-mail was responsible for what happened?
+# othag.hp: How much do you feel that someone aside from the person who wrote the e-mail was responsible for what happened?
+# selfcirc.hp: How much do you feel that circumstances beyond anyone's control are responsible for what happened?
+# targpow.hp: How much do you feel that the person who wrote the e-mail had the power to do something about the situation?
+# othpow.hp: How much do you feel that someone aside from the person who wrote the e-mail had the power to do something about the situation?
+# noonepow.hp: How much do you feel that no one had the hpower to do anything about the situation?
+# sitcont.hp: How much do you feel that the situation was out of anyone's control?
+# immoral.hp: To what extent do you feel what happened was morally wrong?
+# comm.hp: How much do you feel like lack of communication played a role in the situation?
+
+## Perceptions of Target's Appraisals
+## Answer the following questions about what the person who wrote the e-mail thought about the situation that was described (0 = not at all, 4 = extremely)
+# o_pleas.hp: How pleasant did they think it was?
+# o_selfag.hp: How much did they feel responsible for what happened?
+# o_otherag.hp: How much did they feel that someone else was responsible for what happened?
+# o_sitag.hp: How much did they feel that circumstances beyond anyone's control were responsible for what happened?
+# o_selfpow.hp: How much did they feel that they had the power to do something about the situation?
+# o_otherpow.hp: How much did they feel that someone else had the power to do something about the situation?
+# o_noonepow.hp: How much did they feel that no one had the power to do anything about the situation?
+# o_sitpow.hp: How much did they feel that the situation was out of anyone's control?
+# o_immoral.hp: To what extent does the other participant [sic] feel that what happened was morally wrong?
+# o_comm.hp: How much did they feel like lack of communication played a role in the situation?
+
+## Demographics
+# ssc.hp: Subjective social class (ladder measure)
+# female.hp: 0 = male, 1 = female
+## race/ethnicity
+# black.hp, asian.hp, latino.hp, white.hp, other.hp, mideast.hp, natam.hp, pacisl.hp
+# other_secify.hp: Specify other race/ethnicity
+#mom_ed.hp, dad_ed.hp: Parents' highest level of education completed; 1 = less than high school, 2 = high school diploma/GED, 3 = some college, 4 = associate's degree, 7 = bachelor's degree, 8 = postgraduate degree (e.g., Master's, PhD, JD, MD)
+
+# expcomment.hp: Experimenter's comments
+# othernotes.hp: other notes on the subject's data
+# noshow.hp: partner did not show up to the session
+# inattention.hp: subject was not attentive
+# metearly.hp: subjects saw each other early
+# suspicion.hp: subject suspected deception or guessed the purpose of the study
+# misunderstood.hp: subject misunderstood or failed to follow directions
+# other.hp: other possible problems
+# kneweachother.hp: subjects knew each other from outside the lab
+# minorproblem.hp: potential problems, but probably fine
+# language.hp: subject is a non-native English speaker
